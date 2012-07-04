0ab1664556587fc4b2b26c39fabf3518e792f537
Christopher Jones <crjones@gmail.com>
2012-07-04 16:05:26 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 pi-viewer
 =========
 
-Displays pi to an accuracy of 1,000,000. 
+Displays pi to an accuracy of 1,000,000.
+
+This is my first project in Node.js. I used this as a 'hello world' to try to solve the problem expressed at http://3.141592653589793238462643383279502884197169399375105820974944592.com/index3.html.
+
---
+++ web.js
@@ -0,0 +1,41 @@
+express = require('express');
+
+var app = express.createServer();
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
+app.configure(function(){
+    app.set("view options", { layout: false, pretty: true });
+    app.use(express.favicon());
+    app.use(express.static(__dirname + '/public'));
+    }
+);
+
+var nowjs = require("now");
+var everyone = nowjs.initialize(app);
+
+var fs = require('fs');
+
+var buffer = new Buffer(5050);
+
+var fd;
+
+fs.open('pi.txt','r', 0666, function(err, fd_cb) {
+  fd = fd_cb;
+});
+
+everyone.now.nextChunk = function(cursor) {
+    var self = this;
+    fs.read(fd, buffer, 0, 5050, cursor, function(err, bytesRead, buf) {
+    if (bytesRead > 0) {
+      var content = buffer.toString();
+      content = content.replace(/\n/g, "<br />");
+    } else {
+      content = 'EOF'
+    }
+    self.now.appendContent(content);
+  });
+}
